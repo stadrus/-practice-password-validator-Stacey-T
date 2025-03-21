178330931f8cebcf9130b63b9d3d98567dc67386
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-20 20:50:58 -0500

    Loop Praactice

---
+++ main.js
@@ -0,0 +1,27 @@
+const readline = require('readline-sync');
+
+let userName = readline.question("Please enter your name: ")
+console.log(`Hello, ${userName}`);
+
+function validPassword(password){
+let passwordLength = password.length <8;
+let passwordUpperCase = /[A-Z]/.test(password);
+let passwordNumber = /0-10/.test(password);
+
+return passwordLength && passwordUpperCase && passwordNumber;
+
+}
+
+let password;
+do{
+    password = readline.question("Enter a password:(Must be at least 8 characters, 1 upppercase, and one lowercase).");
+    if (!validPassword(password)){;
+    console.log("Invalid Password");
+    }
+} while (!validPassword(password));
+
+console.log("Password accepted");
+
+
+
+
