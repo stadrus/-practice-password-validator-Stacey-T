fa96156f694d853e37a4575720e14e48067e3040
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-20 21:06:26 -0500

    Loops

--- main.js
+++ main.js
@@ -4,23 +4,22 @@ let userName = readline.question("Please enter your name: ")
 console.log(`Hello, ${userName}`);
 
 function validPassword(password){
-let passwordLength = password.length <8;
-let passwordUpperCase = /[A-Z]/.test(password);
-let passwordNumber = /0-10/.test(password);
+    let passwordLength = password.length >=8;
+    let passwordUpperCase = /[A-Z]/.test(password);
+    let passwordNumber = /0-9/.test(password);
 
-return passwordLength && passwordUpperCase && passwordNumber;
+    return passwordLength && passwordUpperCase && passwordNumber;
 
 }
 
 let password;
 do{
-    password = readline.question("Enter a password:(Must be at least 8 characters, 1 upppercase, and one lowercase).");
-    if (!validPassword(password)){;
-    console.log("Invalid Password");
+    password = readline.question("Enter a password:(Must be at least 8 characters, 1 upppercase, and one number).");
+    if (!validPassword(password)) {
+        console.log("Invalid Password");
     }
-} while (!validPassword(password));
-
-console.log("Password accepted");
+} while (validPassword(password));
+    console.log("Password accepted");
 
 
 
